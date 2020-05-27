7d0680e77db5721f93f0ddd5414fc02b39b9ab0c
Vivian Allen <vivian.allen@methods.co.uk>
2020-05-27 17:12:45 +0100

    build_sql handles inserts with trailing statements and commas

--- build_sql.py
+++ build_sql.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def file_to_lines(filepath: str,
                   dir_if_not_in_filepath: str or None = None) -> list:
@@ -23,6 +24,8 @@ def build_sql(template_file, output_file, insert_delimiter='@') -> None:
 
     Assume insert files are all in same dir as template file unless stated
     otherwise
+
+    trailing commas and trailing statemetns (sep by whitespace) are handled
     """
     working_dir = os.path.split(template_file)[0]
 
@@ -34,23 +37,33 @@ def build_sql(template_file, output_file, insert_delimiter='@') -> None:
             output_lines.append(line)
             continue
 
-        line_split_on_delimiter = line.split(insert_delimiter)
+        line_split_on_delimiter = [statement.strip() for statement in
+                                   line.split(insert_delimiter)]
 
-        leading_statment = line_split_on_delimiter[0].strip()
+        leading_statment = line_split_on_delimiter[0]
         if leading_statment != '':
             output_lines.append(leading_statment)
 
         files_to_insert = line_split_on_delimiter[1:]
 
-        # last file to insert may have trailing content after whitespace...
-        end_sequence_of_line = files_to_insert[-1].strip().split(' ', 1)
+        # last file to insert may have trailing content after whitespace
+        end_sequence_of_line = files_to_insert[-1].split(' ', 1)
         files_to_insert[-1] = end_sequence_of_line[0]
 
         for file_to_insert in files_to_insert:
 
-            output_lines.extend(file_to_lines(file_to_insert, working_dir))
+            # handle trailing commas
+            if file_to_insert.endswith(','):
+                lines_to_insert = file_to_lines(file_to_insert[:-1],
+                                                working_dir)
+                lines_to_insert[-1] += ','
 
-        if len(end_sequence_of_line) != 1 :
+            else:
+                lines_to_insert = file_to_lines(file_to_insert, working_dir)
+
+            output_lines.extend(lines_to_insert)
+
+        if len(end_sequence_of_line) != 1:
             trailing_statement = end_sequence_of_line[1]
             output_lines.append(trailing_statement)
 
--- test/test_build_sql.py
+++ test/test_build_sql.py
@@ -29,10 +29,10 @@ def file_to_string_sequence(filepath: str):
 # ===== CASES ===== #
 
 
-def test_build_sql_interpolates_and_writes_to_full_filepath(tmp_path):
+def test_build_sql_interpolates_single_insert_w_trailing_comma(tmp_path):
     """
-    Assert build_sql constructs file given template file referencing files that
-    are simple sql.
+    Assert build_sql constructs file given template file referencing a single
+    insert, with a trailing comma.
     """
     inserted_sql_strs = [
         "SELECT",
@@ -44,7 +44,7 @@ def test_build_sql_interpolates_and_writes_to_full_filepath(tmp_path):
     inserted_sql_filepath = f"{tmp_path}/{inserted_sql_filename}"
 
     template_sql_strs = [
-        f"WITH X AS @{inserted_sql_filename}",
+        f"WITH X AS @{inserted_sql_filename},",
         "SELECT some_stuff FROM somewhere;"
     ]
     template_sql_filename = "template_sql.sql"
@@ -52,7 +52,8 @@ def test_build_sql_interpolates_and_writes_to_full_filepath(tmp_path):
 
     expected_sql_strs = [
         f"WITH X AS",
-        *inserted_sql_strs,
+        *inserted_sql_strs[:-1],
+        f"{inserted_sql_strs[-1]},",
         "SELECT some_stuff FROM somewhere;"
     ]
 
@@ -68,4 +69,153 @@ def test_build_sql_interpolates_and_writes_to_full_filepath(tmp_path):
     returned_sql_strs = file_to_string_sequence(returned_sql_filepath)
 
     assert returned_sql_strs == expected_sql_strs
+
+
+def test_build_sql_interpolates_single_insert_w_trailing_statement(tmp_path):
+    """
+    Assert build_sql constructs file given template file referencing a single
+    insert, with a trailing statement.
+    """
+    inserted_sql_strs = [
+        "SELECT",
+        "   x AS i",
+        "   y AS j",
+        "FROM some_table"
+    ]
+    inserted_sql_filename = "inserted_sql.sql"
+    inserted_sql_filepath = f"{tmp_path}/{inserted_sql_filename}"
+
+    template_sql_strs = [
+        f"WITH X AS @{inserted_sql_filename} AND Y AS Z",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+    template_sql_filename = "template_sql.sql"
+    template_sql_filepath = f"{tmp_path}/{template_sql_filename}"
+
+    expected_sql_strs = [
+        f"WITH X AS",
+        *inserted_sql_strs[:-1],
+        f"{inserted_sql_strs[-1]}",
+        "AND Y AS Z",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+
+    string_sequence_to_file(inserted_sql_strs, inserted_sql_filepath)
+    string_sequence_to_file(template_sql_strs, template_sql_filepath)
+
+    returned_sql_filename ="returned_sql.sql"
+    returned_sql_filepath = f"{tmp_path}/{returned_sql_filename}"
+
+    module_under_test.build_sql(template_file = template_sql_filepath,
+                                output_file = returned_sql_filepath)
+
+    returned_sql_strs = file_to_string_sequence(returned_sql_filepath)
+
+    assert returned_sql_strs == expected_sql_strs
+
+
+def test_build_sql_interpolates_single_insert_w_trailing_statement_and_comma(tmp_path):
+    """
+    Assert build_sql constructs file given template file referencing a single
+    insert, with a trailing comma AND a trailing statement.
+    """
+    inserted_sql_strs = [
+        "SELECT",
+        "   x AS i",
+        "   y AS j",
+        "FROM some_table"
+    ]
+    inserted_sql_filename = "inserted_sql.sql"
+    inserted_sql_filepath = f"{tmp_path}/{inserted_sql_filename}"
+
+    template_sql_strs = [
+        f"WITH X AS @{inserted_sql_filename}, AND Y AS Z",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+    template_sql_filename = "template_sql.sql"
+    template_sql_filepath = f"{tmp_path}/{template_sql_filename}"
+
+    expected_sql_strs = [
+        f"WITH X AS",
+        *inserted_sql_strs[:-1],
+        f"{inserted_sql_strs[-1]},",
+        "AND Y AS Z",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+
+    string_sequence_to_file(inserted_sql_strs, inserted_sql_filepath)
+    string_sequence_to_file(template_sql_strs, template_sql_filepath)
+
+    returned_sql_filename ="returned_sql.sql"
+    returned_sql_filepath = f"{tmp_path}/{returned_sql_filename}"
+
+    module_under_test.build_sql(template_file = template_sql_filepath,
+                                output_file = returned_sql_filepath)
+
+    returned_sql_strs = file_to_string_sequence(returned_sql_filepath)
+
+    assert returned_sql_strs == expected_sql_strs
+
+
+@pytest.mark.skip()
+def test_build_sql_interpolates_multiple_inserts_and_writes(tmp_path):
+    """
+    Assert build_sql constructs file given template file referencing multiple
+    inserts.
+    """
+    inserted_sql_strs_0 = [
+        "SELECT",
+        "   x AS i",
+        "   y AS j",
+        "FROM some_table"
+    ]
+    inserted_sql_0_filename = "inserted_sql.sql"
+    inserted_sql_0_filepath = f"{tmp_path}/{inserted_sql_0_filename}"
+
+    inserted_sql_strs_1 = [
+        "SELECT",
+        "   this AS that",
+        "   the_other AS the_cats_mother",
+        "FROM some_other_table"
+    ]
+    inserted_sql_1_filename = "inserted_sql.sql"
+    inserted_sql_1_filepath = f"{tmp_path}/{inserted_sql_1_filename}"
+
+    inserted_sql_strs_2 = [
+        "SELECT",
+        "   bill AS ben",
+        "   dick AS harry",
+        "FROM yet_another_table"
+    ]
+    inserted_sql_2_filename = "inserted_sql.sql"
+    inserted_sql_2_filepath = f"{tmp_path}/{inserted_sql_2_filename}"
+
+
+    template_sql_strs = [
+        f"WITH X AS @{inserted_sql_filename},",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+    template_sql_filename = "template_sql.sql"
+    template_sql_filepath = f"{tmp_path}/{template_sql_filename}"
+
+    expected_sql_strs = [
+        f"WITH X AS",
+        *inserted_sql_strs,
+        ",",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+
+    string_sequence_to_file(inserted_sql_strs_1, inserted_sql_1_filename)
+    string_sequence_to_file(template_sql_strs, template_sql_filepath)
+
+    returned_sql_filename ="returned_sql.sql"
+    returned_sql_filepath = f"{tmp_path}/{returned_sql_filename}"
+
+    module_under_test.build_sql(template_file = template_sql_filepath,
+                                output_file = returned_sql_filepath)
+
+    returned_sql_strs = file_to_string_sequence(returned_sql_filepath)
+
+    assert returned_sql_strs == expected_sql_strs
+
 
