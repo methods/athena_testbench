fc63139e2d40cb110ecc3d5a7aebcbbb67e2b76e
Vivian Allen <vivian.allen@methods.co.uk>
2020-05-28 14:00:25 +0100

    build_sql raises value error given bad insert statements. Docstrings.

--- build_sql.py
+++ build_sql.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from typing import Sequence
 
@@ -14,7 +15,7 @@ def file_to_lines(filepath: str,
 
     if split_filepath[0] == '':
         if not dir_if_not_in_filepath:
-            raise Exception(
+            raise ValueError(
                 "Either filepath with directory or valid "
                 "dir_if_not_in_filepath must be supplied as args"
             )
@@ -30,12 +31,14 @@ def build_sql(template_file: str,
               insert_delimiter: str = '@',
               suffix_start_chars: Sequence = (',')) -> None:
     """
-    Assemble sql script from template_file and write to output_file. Statements
-    bounded by the insert_delimiter are assumed to point at some other
-    sql script to interpolate in.
+    Assemble sql script from template_file and write to output_file. The
+    template_file can be an absolute path or a path relative to the location
+    that this function is executed from (should be project root).
 
-    e.g., using the default insert_delimiter, this will insert the contents of
-    some_sql_script's 1 to 3 between the @ symbols:
+    Statements bounded by the insert_delimiter are assumed to point at some
+    other sql script to interpolate in. e.g., using the default
+    insert_delimiter, this will insert the contents of some_sql_script's 1 to 3
+    between the @ symbols:
 
         WITH X AS @some_sql_script_1.sql@,
         WITH Y AS @some_sql_script_2.sql@,
@@ -46,19 +49,20 @@ def build_sql(template_file: str,
     filepaths, or path-less filenames. If path-less filenames, they will be
     presumed to be in the same directory as the template_file.
 
-    Statements immediately preceeding an insert statement will be placed on a
+    Statements immediately following an insert statement will be placed on a
     a new line of the compiled script, UNLESS they start with one of the
     suffix_start_chars, in which case they will be appended to the last line
     of the inserted script (e.g. by default trailing comma will be preserved
     as line terminators, rather than placed on new lines).
     """
+    template_file = os.path.abspath(template_file)
     working_dir = os.path.split(template_file)[0]
 
     output_lines = []
 
     def _suffix_or_newline(statement: str) -> None:
         """
-        if statement starts with any of the suffix_start_chars its a suffix,
+        If statement starts with any of the suffix_start_chars its a suffix,
         otherwise it goes in as a newline.
         """
         if any(statement.startswith(char) for char in suffix_start_chars):
@@ -66,11 +70,19 @@ def build_sql(template_file: str,
         else:
             output_lines.append(statement)
 
-    for line in file_to_lines(template_file):
+    for i, line in enumerate(file_to_lines(template_file, working_dir)):
 
-        if insert_delimiter not in line:
+        n_delimiters = len(re.findall(insert_delimiter, line))
+
+        if n_delimiters == 0:
             output_lines.append(line)
             continue
+
+        if n_delimiters % 2 != 0:
+            raise ValueError(
+            f"Unbalanced insert statement found on line {i} of {template_file}"
+            " - insert statements must be bookended by {insert_delimiter}"
+        )
 
         line_split_on_delimiter = line.split(insert_delimiter)
 
@@ -80,18 +92,18 @@ def build_sql(template_file: str,
         if leading_statement != '':
             output_lines.append(leading_statement)
 
-        for i, statement in enumerate(line_split_on_delimiter):
+        for j, statement in enumerate(line_split_on_delimiter):
 
             if statement == '':
                 continue
 
             # even index statements are files to insert
-            if i % 2 == 0:
+            if j % 2 == 0:
                 output_lines.extend(file_to_lines(statement, working_dir))
                 continue
 
-            # odd index statements are interstitial statements, either
-            # or suffixes
+            # odd index statements are interstitial statements, either suffixes
+            # or newlines
             _suffix_or_newline(statement)
 
         if trailing_statement != '':
--- test/test_build_sql.py
+++ test/test_build_sql.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 
 from typing import Sequence
@@ -8,7 +9,7 @@ import build_sql as module_under_test
 # ===== FIXTURES ===== #
 
 
-def string_sequence_to_file(str_seq: Sequence, filepath: str):
+def string_sequence_to_file(str_seq: Sequence, filepath: str) -> None:
     """
     Write string sequence str_seq as lines to filepath.
     """
@@ -16,9 +17,9 @@ def string_sequence_to_file(str_seq: Sequence, filepath: str):
         f.write("\n".join(str_seq))
 
 
-def file_to_string_sequence(filepath: str):
+def file_to_string_sequence(filepath: str) -> list:
     """
-    Write string sequence str_seq as lines to filepath.
+    Load filepath to list of lines
     """
     with open(filepath, 'r') as f:
         return f.read().splitlines()
@@ -190,3 +191,141 @@ def test_build_sql_multiple_inserts(tmp_path: pytest.fixture):
     returned_sql_strs = file_to_string_sequence(returned_sql_filepath)
 
     assert returned_sql_strs == expected_sql_strs
+
+
+def test_build_sql_relative_path():
+    """
+    Assert build_sql constructs file given template file with a path relative
+    to the project root
+    """
+    cwd = "test"
+
+    inserted_sql_strs = [
+        "SELECT",
+        "   x AS i",
+        "   y AS j",
+        "FROM some_table"
+    ]
+    inserted_sql_filename = "inserted_sql.sql"
+    inserted_sql_filepath = f"{cwd}/{inserted_sql_filename}"
+
+    template_sql_strs = [
+        f"WITH X AS @{inserted_sql_filename}@,",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+    template_sql_filename = "template_sql.sql"
+    template_sql_filepath = f"{cwd}/{template_sql_filename}"
+
+    expected_sql_strs = [
+        "WITH X AS ",
+        "SELECT",
+        "   x AS i",
+        "   y AS j",
+        "FROM some_table,",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+
+    string_sequence_to_file(inserted_sql_strs, inserted_sql_filepath)
+    string_sequence_to_file(template_sql_strs, template_sql_filepath)
+
+    returned_sql_filename = "returned_sql.sql"
+    returned_sql_filepath = f"{cwd}/{returned_sql_filename}"
+
+    module_under_test.build_sql(template_file=template_sql_filepath,
+                                output_file=returned_sql_filepath)
+
+    returned_sql_strs = file_to_string_sequence(returned_sql_filepath)
+
+    # make sure those files are removed even if the test fails!
+    try:
+
+        assert returned_sql_strs == expected_sql_strs
+
+    except AssertionError as e:
+
+        raise e
+
+    finally:
+
+        os.remove(inserted_sql_filepath)
+        os.remove(template_sql_filepath)
+        os.remove(returned_sql_filepath)
+
+
+def test_build_sql_project_root():
+    """
+    Assert build_sql constructs file given template file in project root
+    """
+    cwd = os.path.abspath("../")
+
+    inserted_sql_strs = [
+        "SELECT",
+        "   x AS i",
+        "   y AS j",
+        "FROM some_table"
+    ]
+    inserted_sql_filename = "inserted_sql.sql"
+    inserted_sql_filepath = f"{cwd}/{inserted_sql_filename}"
+
+    template_sql_strs = [
+        f"WITH X AS @{inserted_sql_filename}@,",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+    template_sql_filename = "template_sql.sql"
+    template_sql_filepath = f"{cwd}/{template_sql_filename}"
+
+    expected_sql_strs = [
+        "WITH X AS ",
+        "SELECT",
+        "   x AS i",
+        "   y AS j",
+        "FROM some_table,",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+
+    string_sequence_to_file(inserted_sql_strs, inserted_sql_filepath)
+    string_sequence_to_file(template_sql_strs, template_sql_filepath)
+
+    returned_sql_filename = "returned_sql.sql"
+    returned_sql_filepath = f"{cwd}/{returned_sql_filename}"
+
+    module_under_test.build_sql(template_file=template_sql_filepath,
+                                output_file=returned_sql_filepath)
+
+    returned_sql_strs = file_to_string_sequence(returned_sql_filepath)
+
+    # make sure those files are removed even if the test fails!
+    try:
+
+        assert returned_sql_strs == expected_sql_strs
+
+    except AssertionError as e:
+
+        raise e
+
+    finally:
+
+        os.remove(inserted_sql_filepath)
+        os.remove(template_sql_filepath)
+        os.remove(returned_sql_filepath)
+
+
+def test_build_sql_raises_when_inserts_not_bookended(tmp_path: pytest.fixture):
+    """
+    Assert build_sql raises a value error when an insert statment is not
+    bookended by insert delimiters, i.e. @this not @this@.
+    """
+
+    template_sql_strs = [
+        f"WITH X AS @doesnt_matter_what_is_here,",
+        "SELECT some_stuff FROM somewhere;"
+    ]
+    template_sql_filename = "template_sql.sql"
+    template_sql_filepath = f"{tmp_path}/{template_sql_filename}"
+
+    string_sequence_to_file(template_sql_strs, template_sql_filepath)
+
+    with pytest.raises(ValueError):
+
+        module_under_test.build_sql(template_file=template_sql_filepath,
+                                    output_file="doesnt_matter_what_is_here")
