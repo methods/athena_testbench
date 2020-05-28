36a3e1965ae0fee541894124a49fd99e57afd782
Tom Ridd <twridd@gmail.com>
2020-05-28 19:45:42 +0100

    update test generator to create raw la outcomes data

--- test/test_data_generators.py
+++ test/test_data_generators.py
@@ -1,4 +1,5 @@
-from utils.generators import generate_nhs_clean_entry, generate_web_clean_entry, generate_ivr_clean_entry
+from utils.generators import generate_nhs_clean_entry, generate_web_clean_entry, generate_ivr_clean_entry, \
+    generate_raw_la_outcome
 
 
 def test_generate_nhs_clean_entry():
@@ -15,5 +16,11 @@ def test_generate_web_clean_entry():
 
 def test_generate_ivr_clean_entry():
     entry = generate_ivr_clean_entry()
+
+    assert entry is not None
+
+
+def test_generate_raw_la_outcome():
+    entry = generate_raw_la_outcome()
 
     assert entry is not None
