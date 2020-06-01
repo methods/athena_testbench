85cc289d8d7cdc917b3aaeb474439ccecba00e45
Tom Ridd <twridd@gmail.com>
2020-06-01 22:28:54 +0100

    add partial files for wholesaler opt out ... so wholesaler feedback is included

---
+++ sql_to_build/latest_wholesaler_opt_out_stack_TEMPLATE.sql
@@ -0,0 +1,5 @@
+WITH wholesaler_opt_out_list AS (@wholesaler_opt_out_list.sql@)
+
+, wholesaler_opt_out_deduplicated AS (@wholesaler_opt_out_deduplicated.sql@)
+
+@latest_wholesaler_opt_out.sql@
---
+++ sql_to_build/wholesaler_opt_out_list_deduplicated.sql
@@ -0,0 +1,6 @@
+SELECT wholesaler_id, wholesaler_delivery_date, wholesaler_outcome, wholesaler_comment
+FROM (
+       SELECT wholesaler_opt_out_list.*, "row_number"() OVER (PARTITION BY wholesaler_id, wholesaler_delivery_date) rk
+       FROM wholesaler_opt_out_list
+     )
+WHERE rk = 1
