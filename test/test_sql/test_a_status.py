c170146edca3513121eada4dd330e2f5cea17db6
Tom Ridd <twridd@gmail.com>
2020-06-05 19:20:05 +0100

    add tests for status query ... to have confidence

--- test/test_sql/test_a_status.py
+++ test/test_sql/test_a_status.py
@@ -1,4 +1,5 @@
 import datetime
+from operator import itemgetter
 
 import pytest
 from faker import Faker
@@ -8,6 +9,13 @@ from utils.ScenarioBuilder import ScenarioBuilder
 from utils.connections import presto_transaction, pg_connect
 from utils.random_utils import n_days_ago, hash_nhs_number
 
+NHS_NUMBER = 0
+REF_ID = 1
+PROVENANCE = 2
+HAS_ESSENTIALS = 3
+EVENT_DATE = 4
+SYS_TIME_COL = 5
+EVENT_INDEX = 6
 
 # ============= TEST RECORDS ARE JOINING CORRECTLY ================
 
@@ -38,34 +46,113 @@ def test_status_records_limit_to_one_per_person(tmp_path: pytest.fixture):
         assert len(results) == 3
 
 
-def test_status_records_limit_to_one_per_person(tmp_path: pytest.fixture):
+def test_status_returns_most_recent(tmp_path: pytest.fixture):
     with pg_connect() as con:
         scenario_builder = ScenarioBuilder(con)
 
         # GIVEN
-        # multiple ivr records have been added
+        # multiple records have been added between 10 and 1 day ago
         build_and_reset_data_sources(scenario_builder)
 
         add_registration_events(scenario_builder, [
-            registration_event('1', 'IVR', 'NO', n_days_ago(5), n_days_ago(4), 1),
-            registration_event('1', 'IVR', 'YES', n_days_ago(1), n_days_ago(1), 2),
-            registration_event('2', 'IVR', 'NO', n_days_ago(4), n_days_ago(4), 1),
-            registration_event('2', 'IVR', 'YES', n_days_ago(1), n_days_ago(1), 2),
-            registration_event('3', 'IVR', 'NO', n_days_ago(5), n_days_ago(4), 1),
-            registration_event('3', 'IVR', 'YES', n_days_ago(1), n_days_ago(1), 2),
+            registration_event('1', 'IVR', 'NO', n_days_ago(5), n_days_ago(10), 1),
+            registration_event('1', 'WEB', 'YES', n_days_ago(1), n_days_ago(8), 2), # expected
+            registration_event('1', 'IVR', 'NO', n_days_ago(4), n_days_ago(6), 3),
+            registration_event('1', 'local authority', 'YES', n_days_ago(1), n_days_ago(1), 4)
         ])
 
         # WHEN
-        # we build the registration timeline stack and run it in presto
-        query = build_query(tmp_path, 'sql_to_build/registration_status_select.sql', parameters={'timestamp': n_days_ago(2)})
+        # we query for the status 7 days ago
+        query = build_query(tmp_path, 'sql_to_build/registration_status_select.sql', parameters={'timestamp': n_days_ago(7)})
         results = presto_transaction(query)
 
         # THEN
-        # all records are recorded on the registration timeline
-        assert len(results) == 3
+        # we expect to get back the result from 8 days ago
+        assert len(results) == 1
+        assert results[0][EVENT_INDEX] == 2
 
+
+def test_status_returns_most_recent_from_multiple(tmp_path: pytest.fixture):
+    with pg_connect() as con:
+        scenario_builder = ScenarioBuilder(con)
+
+        # GIVEN
+        # multiple records have been added between 10 and 1 day ago
+        build_and_reset_data_sources(scenario_builder)
+
+        add_registration_events(scenario_builder, [
+            registration_event('1', 'IVR', 'NO', n_days_ago(5), n_days_ago(10), 1),
+            registration_event('1', 'WEB', 'YES', n_days_ago(1), n_days_ago(8), 2), # expected
+            registration_event('1', 'IVR', 'NO', n_days_ago(4), n_days_ago(6), 3),
+            registration_event('2', 'WEB', 'NO', n_days_ago(1), n_days_ago(10), 1), # expected
+            registration_event('2', 'local authority', 'YES', n_days_ago(1), n_days_ago(1), 2)
+        ])
+
+        # WHEN
+        # we query for the status 7 days ago
+        query = build_query(tmp_path, 'sql_to_build/registration_status_select.sql', parameters={'timestamp': n_days_ago(7)})
+        results = presto_transaction(query)
+
+        # THEN
+        # we expect to get back the result from 8 days ago
+        assert len(results) == 2
+        results.sort(key=itemgetter(NHS_NUMBER))
+        assert results[0][EVENT_INDEX] == 2
+        assert results[1][EVENT_INDEX] == 1
+
+
+def test_status_returns_none_if_no_event_before_time(tmp_path: pytest.fixture):
+    with pg_connect() as con:
+        scenario_builder = ScenarioBuilder(con)
+
+        # GIVEN
+        # multiple records have been added between 10 and 1 day ago
+        build_and_reset_data_sources(scenario_builder)
+
+        add_registration_events(scenario_builder, [
+            registration_event('1', 'IVR', 'NO', n_days_ago(5), n_days_ago(10), 1),
+            registration_event('1', 'WEB', 'YES', n_days_ago(1), n_days_ago(8), 2),
+            registration_event('1', 'IVR', 'NO', n_days_ago(4), n_days_ago(6), 3),
+        ])
+
+        # WHEN
+        # we query for the status 7 days ago
+        query = build_query(tmp_path, 'sql_to_build/registration_status_select.sql', parameters={'timestamp': n_days_ago(30)})
+        results = presto_transaction(query)
+
+        # THEN
+        # we expect to get no results
+        assert len(results) == 0
+
+
+def test_status_returns_one_status_even_if_two_events_are_latest(tmp_path: pytest.fixture):
+    with pg_connect() as con:
+        scenario_builder = ScenarioBuilder(con)
+
+        # GIVEN
+        # multiple records have been added between 10 and 1 day ago
+        build_and_reset_data_sources(scenario_builder)
+
+        add_registration_events(scenario_builder, [
+            registration_event('1', 'IVR', 'NO', n_days_ago(5), n_days_ago(10), 1),
+            registration_event('1', 'WEB', 'YES', n_days_ago(1), n_days_ago(8), 2), # expected
+            registration_event('1', 'WEB', 'YES', n_days_ago(1), n_days_ago(8), 3), # expected
+            registration_event('1', 'IVR', 'NO', n_days_ago(4), n_days_ago(6), 4),
+            registration_event('1', 'local authority', 'YES', n_days_ago(1), n_days_ago(1), 5)
+        ])
+
+        # WHEN
+        # we query for the status 7 days ago
+        query = build_query(tmp_path, 'sql_to_build/registration_status_select.sql', parameters={'timestamp': n_days_ago(7)})
+        results = presto_transaction(query)
+
+        # THEN
+        # we expect to get back the result from 8 days ago
+        assert len(results) == 1
+        assert results[0][EVENT_INDEX] == 3
 
 # ============= END TESTS ================
+
 
 
 def build_and_reset_data_sources(scenario_builder):
