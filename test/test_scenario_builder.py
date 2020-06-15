48ba59cfbd4640886aef357e1e0a235bd7f8b3e6
Vivian Allen <vivian.allen@methods.co.uk>
2020-06-15 14:54:53 +0100

    managed scenario build fixture hard resets

--- test/test_scenario_builder.py
+++ test/test_scenario_builder.py
@@ -1,30 +1,23 @@
 import pytest
 
-from utils.ScenarioBuilder import ScenarioBuilder
+from utils.ScenarioBuilder import managed_scenario_builder
 from utils.connections import pg_connect
 
 
-def test_scenario_builder_does_populate_database():
-    with pg_connect() as con:
-        scenario_builder = ScenarioBuilder(con)
-        scenario_builder.reset()
-        scenario_builder.insert_random_nhs_records(count=10)
-        scenario_builder.insert_random_ivr_records(count=10)
-        scenario_builder.insert_random_web_records(count=10)
-        scenario_builder.insert_random_la_feedback(count=10)
+def test_scenario_builder_does_populate_database(managed_scenario_builder):
+    managed_scenario_builder.insert_random_nhs_records(count=10)
+    managed_scenario_builder.insert_random_ivr_records(count=10)
+    managed_scenario_builder.insert_random_web_records(count=10)
+    managed_scenario_builder.insert_random_la_feedback(count=10)
 
 
-def test_scenario_builder_builds_arbitrary_table():
-    with pg_connect() as con:
-        scenario_builder = ScenarioBuilder(con)
-        scenario_builder.reset()
+def test_scenario_builder_builds_arbitrary_table(managed_scenario_builder):
+    table_schema = {
+        'test_col_1': 'TEXT',
+        'tes_col_2': 'TEXT',
+    }
+    managed_scenario_builder.build_arbitrary_table("test_table", table_schema)
+    postgres_tables = managed_scenario_builder.list_postgres_tables()
 
-        table_schema = {
-            'test_col_1': 'TEXT',
-            'tes_col_2': 'TEXT',
-        }
-        scenario_builder.build_arbitrary_table("test_table", table_schema)
-        postgres_tables = scenario_builder.list_postgres_tables()
-
-        assert "test_table" in postgres_tables
+    assert "test_table" in postgres_tables
 
--- utils/ScenarioBuilder.py
+++ utils/ScenarioBuilder.py
@@ -1,12 +1,8 @@
-"""
-ToDo:
-    - is the reset feature using TRUNCATE what slows tests down at random?
-"""
-
 import itertools
+import pytest
 import random
 
-from utils.connections import pg_connect
+from utils.connections import pg_connect, presto_transaction
 from utils.generators import generate_nhs_clean_entry, generate_ivr_clean_entry, generate_web_clean_entry, \
     generate_raw_la_outcome
 from utils.random_utils import random_nhs_number
@@ -18,120 +14,183 @@ class ScenarioBuilder:
     def __init__(self, pg_connection):
         self.nhs_number_list = []
         self.con = pg_connection
+        self.init_tables()
+
+    def init_tables(self):
+        self.create_nhs_table()
+        self.create_ivr_table()
+        self.create_web_table()
+        self.create_la_feedback_table()
+
+    def create_nhs_table(self):
+        create_command = f'CREATE TABLE IF NOT EXISTS "nhs_clean_staging" (' \
+            f'nhs_nhs_number TEXT,' \
+            f'nhs_dob TEXT,' \
+            f'nhs_patient_title VARCHAR(256),' \
+            f'nhs_patients_first_name TEXT,' \
+            f'nhs_patients_other_name TEXT,' \
+            f'nhs_patients_surname TEXT,' \
+            f'nhs_patients_address_line1 TEXT,' \
+            f'nhs_patients_address_line2 TEXT,' \
+            f'nhs_patients_address_line3 TEXT,' \
+            f'nhs_patients_address_line4 TEXT,' \
+            f'nhs_patients_address_line5 TEXT,' \
+            f'nhs_postcode TEXT,' \
+            f'nhs_practice_code TEXT,' \
+            f'nhs_practice_name TEXT,' \
+            f'nhs_contact_telephone TEXT,' \
+            f'nhs_deceased TEXT,' \
+            f'nhs_data_source TEXT,' \
+            f'nhs_inception_date TEXT)'
+        self.con.run(create_command)
+        self.con.commit()
 
     def reset_nhs_data(self):
-        with pg_connect() as con:
-            create_command = f'CREATE TABLE IF NOT EXISTS "nhs_clean_staging" (' \
-                f'nhs_nhs_number TEXT,' \
-                f'nhs_dob TEXT,' \
-                f'nhs_patient_title VARCHAR(256),' \
-                f'nhs_patients_first_name TEXT,' \
-                f'nhs_patients_other_name TEXT,' \
-                f'nhs_patients_surname TEXT,' \
-                f'nhs_patients_address_line1 TEXT,' \
-                f'nhs_patients_address_line2 TEXT,' \
-                f'nhs_patients_address_line3 TEXT,' \
-                f'nhs_patients_address_line4 TEXT,' \
-                f'nhs_patients_address_line5 TEXT,' \
-                f'nhs_postcode TEXT,' \
-                f'nhs_practice_code TEXT,' \
-                f'nhs_practice_name TEXT,' \
-                f'nhs_contact_telephone TEXT,' \
-                f'nhs_deceased TEXT,' \
-                f'nhs_data_source TEXT,' \
-                f'nhs_inception_date TEXT)'
-            con.run(create_command)
-            con.run('DELETE FROM "nhs_clean_staging"')
-            con.commit()
+        self.create_nhs_table()
+        self.con.run('DELETE FROM "nhs_clean_staging"')
+        self.con.commit()
+
+    def insert_random_nhs_records(self, count=100):
+        for entry_number in range(0, count):
+            entry = generate_nhs_clean_entry()
+            self.nhs_number_list = self.nhs_number_list + [entry['nhs_nhs_number']]
+            command = self.get_insert_command('nhs_clean_staging', entry)
+            self.con.run(command)
+        self.con.commit()
+
+    def get_nhs_number(self):
+        if len(self.nhs_number_list) == 0:
+            random_nhs_number()
+        else:
+            return random.choice(self.nhs_number_list)
+
+    def create_ivr_table(self):
+        create_command = f'CREATE TABLE IF NOT EXISTS "ivr_clean_staging" (' \
+            f'ivr_nhs_number TEXT,' \
+            f'ivr_postcode TEXT,' \
+            f'ivr_dob TEXT,' \
+            f'ivr_customer_calling_number TEXT,' \
+            f'ivr_current_item_id TEXT,' \
+            f'ivr_transfer TEXT,' \
+            f'ivr_fallback_time TEXT,' \
+            f'ivr_nhs_known TEXT,' \
+            f'ivr_contact_id TEXT,' \
+            f'ivr_preferred_phone_number TEXT,' \
+            f'ivr_postal_code_verified TEXT,' \
+            f'ivr_delivery_supplies TEXT,' \
+            f'ivr_phone_number_calls TEXT,' \
+            f'ivr_carry_supplies TEXT,' \
+            f'ivr_have_help TEXT,' \
+            f'ivr_call_timestamp TEXT,' \
+            f'ivr_unmet_needs TEXT)'
+        self.con.run(create_command)
+        self.con.commit()
 
     def reset_ivr_data(self):
-        with pg_connect() as con:
-            create_command = f'CREATE TABLE IF NOT EXISTS "ivr_clean_staging" (' \
-                f'ivr_nhs_number TEXT,' \
-                f'ivr_postcode TEXT,' \
-                f'ivr_dob TEXT,' \
-                f'ivr_customer_calling_number TEXT,' \
-                f'ivr_current_item_id TEXT,' \
-                f'ivr_transfer TEXT,' \
-                f'ivr_fallback_time TEXT,' \
-                f'ivr_nhs_known TEXT,' \
-                f'ivr_contact_id TEXT,' \
-                f'ivr_preferred_phone_number TEXT,' \
-                f'ivr_postal_code_verified TEXT,' \
-                f'ivr_delivery_supplies TEXT,' \
-                f'ivr_phone_number_calls TEXT,' \
-                f'ivr_carry_supplies TEXT,' \
-                f'ivr_have_help TEXT,' \
-                f'ivr_call_timestamp TEXT,' \
-                f'ivr_unmet_needs TEXT)'
-            con.run(create_command)
-            con.run('DELETE FROM "ivr_clean_staging"')
-            con.commit()
+        self.create_ivr_table()
+        self.con.run('DELETE FROM "ivr_clean_staging"')
+        self.con.commit()
+
+    def insert_random_ivr_records(self, count=100):
+        for entry_number in range(0, count):
+            entry = generate_ivr_clean_entry(nhs_number=self.get_nhs_number())
+            command = self.get_insert_command('ivr_clean_staging', entry)
+            self.con.run(command)
+        self.con.commit()
+
+    def create_web_table(self):
+        create_command = f'CREATE TABLE IF NOT EXISTS "web_clean_staging" (' \
+            f'live_in_england TEXT,' \
+            f'first_name TEXT,' \
+            f'middle_name TEXT,' \
+            f'last_name TEXT,' \
+            f'city TEXT,' \
+            f'address_l1 TEXT,' \
+            f'address_l2 TEXT,' \
+            f'county TEXT,' \
+            f'postcode TEXT,' \
+            f'nhs_number TEXT,' \
+            f'carry_supplies TEXT,' \
+            f'reference_id TEXT,' \
+            f'dob_day TEXT,' \
+            f'dob_month TEXT,' \
+            f'dob_year TEXT,' \
+            f'full_dob TEXT,' \
+            f'phone_number_calls TEXT,' \
+            f'phone_number_texts TEXT,' \
+            f'contact TEXT,' \
+            f'know_nhs_number TEXT,' \
+            f'check_answers_seen TEXT,' \
+            f'nhs_letter TEXT,' \
+            f'basic_care_needs TEXT,' \
+            f'dietary_requirements TEXT,' \
+            f'medical_conditions TEXT,' \
+            f'essential_supplies TEXT,' \
+            f'updated_at NUMERIC,' \
+            f'referenceid TEXT,' \
+            f'unixtimestamp NUMERIC,' \
+            f'created_at NUMERIC)'
+        self.con.run(create_command)
+        self.con.commit()
 
     def reset_web_data(self):
-        with pg_connect() as con:
-            create_command = f'CREATE TABLE IF NOT EXISTS "web_clean_staging" (' \
-                f'live_in_england TEXT,' \
-                f'first_name TEXT,' \
-                f'middle_name TEXT,' \
-                f'last_name TEXT,' \
-                f'city TEXT,' \
-                f'address_l1 TEXT,' \
-                f'address_l2 TEXT,' \
-                f'county TEXT,' \
-                f'postcode TEXT,' \
-                f'nhs_number TEXT,' \
-                f'carry_supplies TEXT,' \
-                f'reference_id TEXT,' \
-                f'dob_day TEXT,' \
-                f'dob_month TEXT,' \
-                f'dob_year TEXT,' \
-                f'full_dob TEXT,' \
-                f'phone_number_calls TEXT,' \
-                f'phone_number_texts TEXT,' \
-                f'contact TEXT,' \
-                f'know_nhs_number TEXT,' \
-                f'check_answers_seen TEXT,' \
-                f'nhs_letter TEXT,' \
-                f'basic_care_needs TEXT,' \
-                f'dietary_requirements TEXT,' \
-                f'medical_conditions TEXT,' \
-                f'essential_supplies TEXT,' \
-                f'updated_at NUMERIC,' \
-                f'referenceid TEXT,' \
-                f'unixtimestamp NUMERIC,' \
-                f'created_at NUMERIC)'
-            con.run(create_command)
-            con.run('DELETE FROM "web_clean_staging"')
-            con.commit()
+        self.create_web_table()
+        self.con.run('DELETE FROM "web_clean_staging"')
+        self.con.commit()
+
+    def insert_random_web_records(self, count=100):
+        for entry_number in range(0, count):
+            entry = generate_web_clean_entry(nhs_number=self.get_nhs_number())
+            command = self.get_insert_command('web_clean_staging', entry)
+            self.con.run(command)
+        self.con.commit()
+
+    def create_la_feedback_table(self):
+        create_command = f'CREATE TABLE IF NOT EXISTS "raw_la_outcomes_staging" (' \
+            f'reference TEXT,' \
+            f'id TEXT,' \
+            f'sequencenumber NUMERIC,' \
+            f'nhsnumber TEXT,' \
+            f'firstname TEXT,' \
+            f'middlename TEXT,' \
+            f'lastname TEXT,' \
+            f'dateofbirth TEXT,' \
+            f'localauthoritydistrict TEXT,' \
+            f'postcode TEXT,' \
+            f'servicecode TEXT,' \
+            f'servicename TEXT,' \
+            f'gdsoutcomecode TEXT,' \
+            f'gdsoutcomedescription TEXT,' \
+            f'localoutcomecode TEXT,' \
+            f'completedoutcomedatetime TEXT,' \
+            f'nextplanneddate TEXT,' \
+            f'outcomecomments TEXT,' \
+            f'inputoutcomecode TEXT,' \
+            f'inputcompletedoutcomedate TEXT,' \
+            f'inputoutcomecomments TEXT)'
+        self.con.run(create_command)
+        self.con.commit()
 
     def reset_la_feedback_data(self):
-        with pg_connect() as con:
-            create_command = f'CREATE TABLE IF NOT EXISTS "raw_la_outcomes_staging" (' \
-                f'reference TEXT,' \
-                f'id TEXT,' \
-                f'sequencenumber NUMERIC,' \
-                f'nhsnumber TEXT,' \
-                f'firstname TEXT,' \
-                f'middlename TEXT,' \
-                f'lastname TEXT,' \
-                f'dateofbirth TEXT,' \
-                f'localauthoritydistrict TEXT,' \
-                f'postcode TEXT,' \
-                f'servicecode TEXT,' \
-                f'servicename TEXT,' \
-                f'gdsoutcomecode TEXT,' \
-                f'gdsoutcomedescription TEXT,' \
-                f'localoutcomecode TEXT,' \
-                f'completedoutcomedatetime TEXT,' \
-                f'nextplanneddate TEXT,' \
-                f'outcomecomments TEXT,' \
-                f'inputoutcomecode TEXT,' \
-                f'inputcompletedoutcomedate TEXT,' \
-                f'inputoutcomecomments TEXT)'
-            con.run(create_command)
-            con.run('DELETE FROM "raw_la_outcomes_staging"')
-            con.commit()
+        self.create_la_feedback_table()
+        self.con.run('DELETE FROM "raw_la_outcomes_staging"')
+        self.con.commit()
+
+    def insert_random_la_feedback(self, count=100):
+        for entry_number in range(0, count):
+            entry = generate_raw_la_outcome(nhs_number=self.get_nhs_number())
+            command = self.get_insert_command('raw_la_outcomes_staging', entry)
+            self.con.run(command)
+        self.con.commit()
+
+    def insert_la_feedback(self, nhsnumber, inputoutcomecode, inputcompletedoutcomedate, inputoutcomecomments):
+        entry = generate_raw_la_outcome(nhs_number=nhsnumber)
+        entry['inputoutcomecode'] = inputoutcomecode
+        entry['inputcompletedoutcomedate'] = inputcompletedoutcomedate
+        entry['inputoutcomecomments'] = inputoutcomecomments
+        command = self.get_insert_command('raw_la_outcomes_staging', entry)
+        self.con.run(command)
+        self.con.commit()
 
     def get_insert_command(self, table_name, record):
         fields = ','.join([f'"{key}"' for key in record])
@@ -161,65 +220,22 @@ class ScenarioBuilder:
             self.con.run(insert_command)
         self.con.commit()
 
-
     def drop_arbitrary_tables(self):
         for table in self.arbitrary_tables:
             self.con.run(f"DROP TABLE IF EXISTS {table}")
         self.con.commit()
         self.arbitrary_tables = []
 
-
-    def reset(self):
-        self.reset_nhs_data()
-        self.reset_ivr_data()
-        self.reset_web_data()
-        self.reset_la_feedback_data()
-
-
-    def insert_random_nhs_records(self, count=100):
-        for entry_number in range(0, count):
-            entry = generate_nhs_clean_entry()
-            self.nhs_number_list = self.nhs_number_list + [entry['nhs_nhs_number']]
-            command = self.get_insert_command('nhs_clean_staging', entry)
-            self.con.run(command)
+    def reset_arbitrary_tables(self):
+        for table in self.arbitrary_tables:
+            self.con.run(f'DELETE FROM "{table_name}"')
         self.con.commit()
 
-    def get_nhs_number(self):
-        if len(self.nhs_number_list) == 0:
-            random_nhs_number()
-        else:
-            return random.choice(self.nhs_number_list)
-
-    def insert_random_ivr_records(self, count=100):
-        for entry_number in range(0, count):
-            entry = generate_ivr_clean_entry(nhs_number=self.get_nhs_number())
-            command = self.get_insert_command('ivr_clean_staging', entry)
-            self.con.run(command)
-        self.con.commit()
-
-    def insert_random_web_records(self, count=100):
-        for entry_number in range(0, count):
-            entry = generate_web_clean_entry(nhs_number=self.get_nhs_number())
-            command = self.get_insert_command('web_clean_staging', entry)
-            self.con.run(command)
-        self.con.commit()
-
-    def insert_random_la_feedback(self, count=100):
-        for entry_number in range(0, count):
-            entry = generate_raw_la_outcome(nhs_number=self.get_nhs_number())
-            command = self.get_insert_command('raw_la_outcomes_staging', entry)
-            self.con.run(command)
-        self.con.commit()
-
-    def insert_la_feedback(self, nhsnumber, inputoutcomecode, inputcompletedoutcomedate, inputoutcomecomments):
-
-        entry = generate_raw_la_outcome(nhs_number=nhsnumber)
-        entry['inputoutcomecode'] = inputoutcomecode
-        entry['inputcompletedoutcomedate'] = inputcompletedoutcomedate
-        entry['inputoutcomecomments'] = inputoutcomecomments
-        command = self.get_insert_command('raw_la_outcomes_staging', entry)
-        self.con.run(command)
-        self.con.commit()
+    def kill_running_presto_queries(self):
+        all_queries = presto_transaction("SELECT * FROM system.runtime.queries")
+        running_query_ids = (query[1] for query in all_queries if 'RUNNING' in query)
+        for query_id in running_query_ids:
+            presto_transaction(f"CALL system.runtime.kill_query(query_id => '{query_id}')")
 
     def list_postgres_tables(self) -> list:
         """
@@ -236,3 +252,34 @@ class ScenarioBuilder:
         self.con.commit()
 
         return list(itertools.chain(*tables))
+
+    def get_connection():
+        return self.con
+
+    def reset(self):
+        self.kill_running_presto_queries()
+        self.reset_nhs_data()
+        self.reset_ivr_data()
+        self.reset_web_data()
+        self.reset_la_feedback_data()
+        self.reset_arbitrary_tables()
+
+    def hard_reset(self):
+        self.kill_running_presto_queries()
+        self.reset_nhs_data()
+        self.reset_ivr_data()
+        self.reset_web_data()
+        self.reset_la_feedback_data()
+        self.drop_arbitrary_tables()
+
+
+@pytest.fixture()
+def managed_scenario_builder():
+    """
+    Auto-resetting, instantiated scenario builder with managed connection as
+    pytest fixture.
+    """
+    with pg_connect() as con:
+        scenario_builder = ScenarioBuilder(con)
+        yield scenario_builder
+        scenario_builder.hard_reset()
